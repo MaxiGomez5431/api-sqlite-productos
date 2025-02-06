6cdcb23af783979543ecfa1e4a6aaba3d68f8ff1
MaxiGomez5431 <mamitgomez@gmail.com>
2025-02-06 17:30:34 -0300

    added / route

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const db = require('./models');
 const app = express();
-const seed = require('./seeds/seed')
+const seed = require('./seeds/seed');
 
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Base funcionando correctamente');
+});
 
 app.use(routes.productosRoute);
 app.use(routes.fabricantesRoute);
 app.use(routes.componentesRoute);
 
 app.listen(PORT, async () => {
-    await seed()
-    console.log(`Aplicación iniciada en el puerto ${PORT}`);
+  //await seed();
+  console.log(`Aplicación iniciada en el puerto ${PORT}`);
 });
